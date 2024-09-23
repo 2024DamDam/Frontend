5a29734471b92f3edafc15bc0fe9d95d08d55774
JIHOON CHOI <jihoon2110@sm.ac.kr>
2024-09-24 03:21:06 +0900

    캐릭터 음성 완성

--- src/components/ChooseCh5.js
+++ src/components/ChooseCh5.js
@@ -4,10 +4,19 @@ import Navbar from './Navbar';
 import '../styles/ChooseCh.css';
 import profileImage from '../photo/박보검.png'; // 박보검 프로필 사진
 
+const sendCharacterChoice = () => {
+  const voiceId = "6G2q0bCZZiM3WmPMHNXM";  
+
+  // voice_id를 localStorage에 저장
+  localStorage.setItem('voice_id', voiceId);
+};
+
 const ChooseCh5 = () => {
   const navigate = useNavigate();
 
-  const handleStartClick = () => {
+  const handleCharacterClick = () => {
+    // voice_id를 저장한 후 chat 페이지로 이동
+    sendCharacterChoice();
     navigate('/chat');
   };
 
@@ -22,7 +31,7 @@ const ChooseCh5 = () => {
             <img src={profileImage} alt="Profile" />
           </div>
           <div className="tags">
-            <span>#배우</span>
+          <span>#배우</span>
             <span>#훈훈한미소</span>
             <span>#다정다감</span>
           </div>
@@ -30,7 +39,7 @@ const ChooseCh5 = () => {
             <p>안녕하세요, 박보검입니다.</p>
             <p>오늘도 즐거운 하루 보내세요!</p>
           </div>
-          <button className="start-button" onClick={handleStartClick}>START</button>
+          <button className="start-button" onClick={handleCharacterClick}>START</button>
         </div>
       </main>
     </div>
